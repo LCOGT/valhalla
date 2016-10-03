18ead9f10a72cbf7623ae42a54fa50ffd38f8fd3
Jon Nation <jnation@lcogt.net>
2016-10-03 22:26:32 +0000

    added test for weird floyds default acquire mode

--- valhalla/userrequests/test_api.py
+++ valhalla/userrequests/test_api.py
@@ -118,6 +118,19 @@ class TestUserPostRequestApi(APITestCase):
         response = self.client.post(reverse('api:user_requests-list'), data=bad_data)
         self.assertEqual(response.status_code, 400)
 
+    def test_post_userrequest_default_acquire_mode(self):
+        bad_data = self.generic_payload.copy()
+        # verify default acquire mode is 'optional' for non-floyds
+        response = self.client.post(reverse('api:user_requests-list'), data=bad_data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['requests'][0]['target']['acquire_mode'], 'OPTIONAL')
+
+        # check that default acquire mode is 'on' for floyds
+        bad_data['requests'][0]['molecules'][0]['instrument_name'] = '2M0-FLOYDS-SCICAM'
+        response = self.client.post(reverse('api:user_requests-list'), data=bad_data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()['requests'][0]['target']['acquire_mode'], 'ON')
+
 
 class TestSiderealTarget(APITestCase):
     def setUp(self):
