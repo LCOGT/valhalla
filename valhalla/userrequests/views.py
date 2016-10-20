fa9efb89e51c2969612a542cc48c3b73aed60516
Jon Nation <jnation@lcogt.net>
2016-10-20 17:43:21 +0000

    added temporary endpoints for telescope states data

--- valhalla/userrequests/views.py
+++ valhalla/userrequests/views.py
@@ -1,4 +1,8 @@
 from django_filters.views import FilterView
+from rest_framework.response import Response
+from datetime import datetime, timedelta
+from rest_framework.decorators import api_view
+from valhalla.common.telescope_states import get_telescope_states, get_telescope_availability_per_day
 
 from valhalla.userrequests.models import UserRequest
 from valhalla.userrequests.filters import UserRequestFilter
@@ -14,3 +18,34 @@ class UserRequestListView(FilterView):
             return UserRequest.objects.filter(proposal__in=self.request.user.proposal_set.all())
         else:
             return UserRequest.objects.none()
+
+
+@api_view(['GET',])
+def telescope_states(request):
+    '''
+
+    :param request:
+    :return:
+    '''
+    start = datetime.strptime(request.query_params.get('start', '2016-10-10 0:0:0'), '%Y-%m-%d %H:%M:%S')
+    end = datetime.strptime(request.query_params.get('end', '2016-10-16 0:0:0'), '%Y-%m-%d %H:%M:%S')
+    telescope_states = get_telescope_states(start, end)
+    str_telescope_states = {str(k): v for k, v in telescope_states.items()}
+
+    return Response(str_telescope_states)
+
+
+@api_view(['GET',])
+def telescope_availability(request):
+    '''
+
+    :param request:
+    :return:
+    '''
+    start = datetime.strptime(request.query_params.get('start', '2016-10-10 0:0:0'), '%Y-%m-%d %H:%M:%S')
+    end = datetime.strptime(request.query_params.get('end', '2016-10-16 0:0:0'), '%Y-%m-%d %H:%M:%S')
+    telescope_availability = get_telescope_availability_per_day(start - timedelta(days=1), end + timedelta(days=1))
+    str_telescope_availability = {str(k): v for k, v in telescope_availability.items()}
+
+    return Response(str_telescope_availability)
+
