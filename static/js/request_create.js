9058ab3fd950e54d7cb4fc85eced9218c5824e4a
Austin Riba <ariba@lco.global>
2016-12-02 16:20:25 -0800

    partially working form validation frontend+backend

--- static/js/request_create.js
+++ static/js/request_create.js
@@ -1,45 +1,50 @@
 /* globals $ Vue */
-
+/* Notes
+ * vee-validate seems nice, but getting it to work with nested forms may be an issue. Can't seem to index in templates.
+ * May be worth just writing our own error bag class that just extends what the backend sends us.
+ * See the validate method below
+ * we may also be able to just connect custom validators to every field that needs it an parse the josn response
+ */
+var userrequest = {
+  proposal: '',
+  group_id: '',
+  operator: 'SINGLE',
+  ipp_value: 1.0,
+  observation_type: 'NORMAL',
+  requests:[{
+    target: {
+      name: '',
+      type: 'SIDEREAL',
+      ra: '',
+      dec: ''
+    },
+    molecules:[{
+      type: 'EXPOSE',
+      instrument_name: '',
+      fitler: '',
+      exposure_time: 0,
+      exposure_count: 1,
+      bin_x: 1,
+      bin_y: 1
+    }],
+    windows:[{
+      start: '2016-09-29T21:12:18Z',
+      end: '2016-10-29T21:12:19Z'
+    }],
+    location:{
+      telescope_class: '1m0'
+    },
+    constraints: {
+      max_airmass: 2.0,
+      min_lunar_distance: 30.0
+    }
+  }]
+}
 var app = new Vue({
   el: '#vueapp',
   data: {
-    errors: {},
     proposals: [],
-    userrequest: {
-      proposal: '',
-      group_id: '',
-      operator: 'SINGLE',
-      ipp_value: 1.0,
-      observation_type: 'NORMAL',
-      requests:[{
-        target: {
-          name: '',
-          type: 'SIDEREAL',
-          ra: '',
-          dec: ''
-        },
-        molecules:[{
-          type: 'EXPOSE',
-          instrument_name: '',
-          fitler: '',
-          exposure_time: 0,
-          exposure_count: 1,
-          bin_x: 1,
-          bin_y: 1
-        }],
-        windows:[{
-          start: '2016-09-29T21:12:18Z',
-          end: '2016-10-29T21:12:19Z'
-        }],
-        location:{
-          telescope_class: '1m0'
-        },
-        constraints: {
-          max_airmass: 2.0,
-          min_lunar_distance: 30.0
-        }
-      }]
-    }
+    userrequest: JSON.parse(JSON.stringify(userrequest))
   },
   methods: {
     validate: function(){
@@ -50,7 +55,13 @@ var app = new Vue({
         data: JSON.stringify(that.userrequest),
         contentType: 'application/json',
         success: function(data){
-          that.errors = data;
+          for (var key in data){
+            if(data.hasOwnProperty(key)){
+              for(i=0; i<data[key].length; i++){
+                that.errors.add(key, data[key][i]);
+              }
+            }
+          }
         }
       });
     },
@@ -59,6 +70,9 @@ var app = new Vue({
       $.getJSON('/api/profile/', function(data){
         that.proposals = data.proposals;
       });
+    },
+    addRequest: function(){
+      this.userrequest.requests.push(JSON.parse(JSON.stringify(userrequest.requests[0])))
     }
   }
 });
