f138895d4521d90667f720202bf46da935de45e7
Austin Riba <ariba@lco.global>
2017-01-12 11:59:33 -0800

    POC up to ur level

--- static/js/request_create.js
+++ static/js/request_create.js
@@ -1,5 +1,47 @@
 /* globals _ $ Vue moment */
 
+var userrequest = {
+  proposal: undefined,
+  group_id: undefined,
+  operator: 'SINGLE',
+  ipp_value: 1.0,
+  observation_type: 'NORMAL',
+  requests:[{
+    data_type: '',
+    instrument_name: '',
+    target: {
+      name: undefined,
+      type: 'SIDEREAL',
+      ra: undefined,
+      dec: undefined,
+      scheme: 'MPC_MINOR_PLANET'
+    },
+    molecules:[{
+      type: 'EXPOSE',
+      instrument_name: undefined,
+      fitler: '',
+      exposure_time: 30,
+      exposure_count: 1,
+      bin_x: 1,
+      bin_y: 1,
+      fill_window: false,
+      acquire_mode: undefined,
+      acquire_radius_arcsec: undefined,
+    }],
+    windows:[{
+      start: moment().format('YYYY-M-D HH:mm:ss'),
+      end: moment().format('YYYY-M-D HH:mm:ss')
+    }],
+    location:{
+      telescope_class: undefined
+    },
+    constraints: {
+      max_airmass: 2.0,
+      min_lunar_distance: 30.0
+    }
+  }]
+};
+
 Vue.component('window',{
   props: ['istart', 'iend'],
   data: function(){
@@ -7,8 +49,7 @@ Vue.component('window',{
   },
   methods: {
     update: function(){
-      console.log(this.$data);
-      this.$emit('windowUpdate', this.$data);
+      this.$emit('windowupdate', this.$data);
     }
   },
   template: '#window-template'
@@ -17,60 +58,42 @@ Vue.component('window',{
 Vue.component('request', {
   props: ['iwindows'],
   data: function(){
-    console.log('wut')
-    console.log(this.iwindows)
     return { windows: this.iwindows };
   },
   methods: {
     windowUpdated: function(data){
-      this.$root.$data.requests[0].windows[0].start = data.start;
-      this.$root.$data.requests[0].windows[0].end = data.end;
+      this.windows[0] = data;
+      this.$emit('requestupdate', this.$data);
     }
   },
   template: '#request-template'
 });
 
+Vue.component('userrequest', {
+  props: ['irequests'],
+  data: function(){
+    return { requests: this.irequests };
+  },
+  methods: {
+    requestUpdated: function(data){
+      this.requests[0] = data;
+      this.$emit('userrequestupdate', this.$data);
+      console.log(this.requests[0].windows[0].start)
+      this.$root.$data.userrequest.requests[0] = this.requests[0];
+    }
+  },
+  template: '#userrequest-template'
+});
+
 var vm = new Vue({
   el: '#vueapp',
   data:{
-    proposal: undefined,
-    group_id: undefined,
-    operator: 'SINGLE',
-    ipp_value: 1.0,
-    observation_type: 'NORMAL',
-    requests:[{
-      data_type: '',
-      instrument_name: '',
-      target: {
-        name: undefined,
-        type: 'SIDEREAL',
-        ra: undefined,
-        dec: undefined,
-        scheme: 'MPC_MINOR_PLANET'
-      },
-      molecules:[{
-        type: 'EXPOSE',
-        instrument_name: undefined,
-        fitler: '',
-        exposure_time: 30,
-        exposure_count: 1,
-        bin_x: 1,
-        bin_y: 1,
-        fill_window: false,
-        acquire_mode: undefined,
-        acquire_radius_arcsec: undefined,
-      }],
-      windows:[{
-        start: moment().format('YYYY-M-D HH:mm:ss'),
-        end: moment().format('YYYY-M-D HH:mm:ss')
-      }],
-      location:{
-        telescope_class: undefined
-      },
-      constraints: {
-        max_airmass: 2.0,
-        min_lunar_distance: 30.0
-      }
-    }]
+    userrequest: userrequest
+  },
+  methods: {
+    userrequestUpdated: function(data){
+      console.log(data);
+      this.userrequest = data;
+    }
   }
 });
