e68095c31a9e618be63c092a66c503437b6c15c1
Austin Riba <ariba@lco.global>
2017-02-07 16:11:31 -0800

    add sidenav styles to sidenav component

--- static/js/components/sidenav.vue
+++ static/js/components/sidenav.vue
@@ -28,3 +28,59 @@
     props: ['userrequest'],
   };
 </script>
+<style>
+/* sidebar */
+.bs-docs-sidebar {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    position: fixed;
+}
+
+/* all links */
+.bs-docs-sidebar .nav>li>a {
+    color: #999;
+    border-left: 2px solid transparent;
+    padding: 4px 20px;
+    font-size: 16px;
+    font-weight: 400;
+}
+
+/* nested links */
+.bs-docs-sidebar .nav .nav>li>a {
+    padding-top: 1px;
+    padding-bottom: 1px;
+    padding-left: 30px;
+    font-size: 14px;
+}
+
+/* active & hover links */
+.bs-docs-sidebar .nav>.active>a,
+.bs-docs-sidebar .nav>li>a:hover,
+.bs-docs-sidebar .nav>li>a:focus {
+    color: #009ec3;
+    text-decoration: none;
+    background-color: transparent;
+    border-left-color: #31b0d5;
+}
+/* all active links */
+.bs-docs-sidebar .nav>.active>a,
+.bs-docs-sidebar .nav>.active:hover>a,
+.bs-docs-sidebar .nav>.active:focus>a {
+    font-weight: 700;
+}
+/* nested active links */
+.bs-docs-sidebar .nav .nav>.active>a,
+.bs-docs-sidebar .nav .nav>.active:hover>a,
+.bs-docs-sidebar .nav .nav>.active:focus>a {
+    font-weight: 500;
+}
+
+/* hide inactive nested list */
+.bs-docs-sidebar .nav ul.nav {
+    display: none;
+}
+/* show active nested list */
+.bs-docs-sidebar .nav>.active>ul.nav {
+    display: block;
+}
+</style>
