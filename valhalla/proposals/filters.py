40d2ac1890bbf4896ceb1965ffc38773b5c0102e
Austin Riba <ariba@lco.global>
2017-02-14 15:30:56 -0800

    remove memberships if pi, show hide ci list

--- valhalla/proposals/filters.py
+++ valhalla/proposals/filters.py
@@ -7,7 +7,7 @@ class ProposalFilter(django_filters.FilterSet):
     semester = django_filters.ModelChoiceFilter(
         label="Semester", distinct=True, queryset=Semester.objects.all().order_by('-start')
     )
-    active = django_filters.ChoiceFilter(choices=((0, 'Inactive'), (1, 'Active')))
+    active = django_filters.ChoiceFilter(choices=((0, 'Inactive'), (1, 'Active')), empty_label='All')
 
     class Meta:
         model = Proposal
