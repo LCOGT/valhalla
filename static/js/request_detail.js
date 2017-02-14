ccb04422d80f9f8a89c45fe7f9d3b5d75c9326fa
Austin Riba <ariba@lco.global>
2017-02-14 12:33:13 -0800

    handle setting up archive api token a bit better

--- static/js/request_detail.js
+++ static/js/request_detail.js
@@ -1,10 +1,8 @@
 import $ from 'jquery';
 import Vue from 'vue';
 
-import {ajaxSetup, login, getThumbnail, getLatestFrame, downloadAll} from './archive.js';
+import {getThumbnail, getLatestFrame, downloadAll} from './archive.js';
 import App from './request_detail.vue';
-
-ajaxSetup();
 
 new Vue({
   el: '#app',
@@ -18,19 +16,17 @@ $('#downloadall').click(function(){
 });
 
 $(document).ready(function(){
-  login(function(){
-    $('.thumbnail-small').each(function(idx, elem){
-      getLatestFrame($(elem).data('request'), function(frame){
-        $(elem).fadeOut(200);
-        $(elem).prev().show().html('<center><span class="fa fa-spinner fa-spin"></span></center>');
-        getThumbnail(frame.id, 100, function(data){
-          if(data.error){
-            $(elem).prev().html(data.error);
-          }else{
-            $(elem).attr('src', data.url).show();
-            $(elem).prev().hide();
-          }
-        });
+  $('.thumbnail-small').each(function(idx, elem){
+    getLatestFrame($(elem).data('request'), function(frame){
+      $(elem).fadeOut(200);
+      $(elem).prev().show().html('<center><span class="fa fa-spinner fa-spin"></span></center>');
+      getThumbnail(frame.id, 100, function(data){
+        if(data.error){
+          $(elem).prev().html(data.error);
+        }else{
+          $(elem).attr('src', data.url).show();
+          $(elem).prev().hide();
+        }
       });
     });
   });
--- static/js/userrequest_detail.js
+++ static/js/userrequest_detail.js
@@ -1,27 +1,23 @@
 import $ from 'jquery';
 
-import {ajaxSetup, login, getThumbnail, getLatestFrame, downloadAll} from './archive.js';
-
-ajaxSetup();
+import {getThumbnail, getLatestFrame, downloadAll} from './archive.js';
 
 $('#downloadall').click(function(){
   downloadAll($(this).data('requestid'));
 });
 
 $(document).ready(function(){
-  login(function(){
-    $('.thumbnail-small').each(function(idx, elem){
-      getLatestFrame($(elem).data('request'), function(frame){
-        $(elem).fadeOut(200);
-        $(elem).prev().show().html('<center><span class="fa fa-spinner fa-spin"></span></center>');
-        getThumbnail(frame.id, 100, function(data){
-          if(data.error){
-            $(elem).prev().html(data.error);
-          }else{
-            $(elem).attr('src', data.url).show();
-            $(elem).prev().hide();
-          }
-        });
+  $('.thumbnail-small').each(function(idx, elem){
+    getLatestFrame($(elem).data('request'), function(frame){
+      $(elem).fadeOut(200);
+      $(elem).prev().show().html('<center><span class="fa fa-spinner fa-spin"></span></center>');
+      getThumbnail(frame.id, 100, function(data){
+        if(data.error){
+          $(elem).prev().html(data.error);
+        }else{
+          $(elem).attr('src', data.url).show();
+          $(elem).prev().hide();
+        }
       });
     });
   });
