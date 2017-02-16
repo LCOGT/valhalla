e2601ee9bbb32a38f6d16ecd97052fb0766c09e7
Austin Riba <ariba@lco.global>
2017-02-16 10:40:50 -0800

    factor request row js into single file

--- static/js/request_detail.js
+++ static/js/request_detail.js
@@ -1,7 +1,6 @@
-import $ from 'jquery';
 import Vue from 'vue';
 
-import {getThumbnail, getLatestFrame, downloadAll} from './archive.js';
+import './request_row';
 import App from './request_detail.vue';
 
 new Vue({
@@ -9,26 +8,5 @@ new Vue({
   render: function(h){
     return h(App);
   }
-});
-
-$('#downloadall').click(function(){
-  downloadAll($(this).data('requestid'));
-});
-
-$(document).ready(function(){
-  $('.thumbnail-small').each(function(idx, elem){
-    getLatestFrame($(elem).data('request'), function(frame){
-      $(elem).fadeOut(200);
-      $(elem).prev().show().html('<center><span class="fa fa-spinner fa-spin"></span></center>');
-      getThumbnail(frame.id, 100, function(data){
-        if(data.error){
-          $(elem).prev().html(data.error);
-        }else{
-          $(elem).attr('src', data.url).show();
-          $(elem).prev().hide();
-        }
-      });
-    });
-  });
 });
 
