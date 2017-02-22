bb2205acbdb80390343bfda04562d3357ba5104d
Jon Nation <jnation@lcogt.net>
2017-02-22 19:30:57 +0000

    added active block to telescope states plot

--- static/js/components/airmass_telescope_states.vue
+++ static/js/components/airmass_telescope_states.vue
@@ -2,7 +2,7 @@
   <div class="airmassTelescopeStatesCombo">
     <airmass ref="airmass" v-on:rangechanged="updateTelescopeStatesRange" :data="airmassData" v-show="'airmass_limit' in airmassData" :showZoomControls="true" :alignleft="true"></airmass>
     <p></p>
-    <telescope_states ref="telescope_states" v-on:rangechanged="updateAirmassRange" :data="telescopeStatesData" v-show="'airmass_limit' in airmassData" :showZoomControls="false"></telescope_states>
+    <telescope_states ref="telescope_states" v-on:rangechanged="updateAirmassRange" :data="telescopeStatesData" :activeBlock="activeBlock" v-show="'airmass_limit' in airmassData" :showZoomControls="false"></telescope_states>
   </div>
 </template>
 <script>
@@ -10,7 +10,7 @@ import airmass from './airmass.vue';
 import telescope_states from './telescope_states.vue';
 
 export default {
-  props: ['airmassData', 'telescopeStatesData'],
+  props: ['airmassData', 'telescopeStatesData', 'activeBlock'],
   components: {airmass, telescope_states},
   data: function(){
     return {};
