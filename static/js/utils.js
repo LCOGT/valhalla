84584a44b9c52505c278dee0123d5d91274b0718
Jon Nation <jnation@lcogt.net>
2017-03-01 01:29:29 +0000

    fixed sexagesimal js

--- static/js/utils.js
+++ static/js/utils.js
@@ -21,16 +21,15 @@ function semesterEnd(datetime){
 function sexagesimalRaToDecimal(ra) {
   // algorithm: ra_decimal = 15 * ( hh + mm/60 + ss/(60 * 60) )
   /*                 (    hh     ):(     mm            ):  (   ss  ) */
-  if(ra == undefined){
-    return '';
-  }
-  var m = ra.match('^([0-9]?[0-9]):([0-5]?[0-9][.0-9]*):?([.0-9]+)?$');
-  if(m){
-    var hh = parseInt(m[1], 10);
-    var mm = parseFloat(m[2]);
-    var ss = m[3] ? parseFloat(m[3]) : 0.0;
-    if (hh >= 0 && hh <= 23 && mm >= 0 && mm < 60 && ss >= 0 && ss < 60){
-      ra = (15.0 * (hh + mm / 60.0 + ss / (3600.0))).toFixed(10);
+  if(typeof ra === 'string') {
+    var m = ra.match('^([0-9]?[0-9]):([0-5]?[0-9][.0-9]*):?([.0-9]+)?$');
+    if (m) {
+      var hh = parseInt(m[1], 10);
+      var mm = parseFloat(m[2]);
+      var ss = m[3] ? parseFloat(m[3]) : 0.0;
+      if (hh >= 0 && hh <= 23 && mm >= 0 && mm < 60 && ss >= 0 && ss < 60) {
+        ra = (15.0 * (hh + mm / 60.0 + ss / (3600.0))).toFixed(10);
+      }
     }
   }
   return ra;
@@ -39,17 +38,16 @@ function sexagesimalRaToDecimal(ra) {
 function sexagesimalDecToDecimal(dec){
   // algorithm: dec_decimal = sign * ( dd + mm/60 + ss/(60 * 60) )
   /*                  ( +/-   ) (    dd     ):(     mm            ): (   ss   ) */
-  if(dec == undefined){
-    return '';
-  }
-  var m = dec.match('^([+-])?([0-9]?[0-9]):([0-5]?[0-9][.0-9]*):?([.0-9]+)?$');
-  if(m){
-    var sign = m[1] === '-' ? -1 : 1;
-    var dd = parseInt(m[2], 10);
-    var mm = parseFloat(m[3]);
-    var ss = m[4] ? parseFloat(m[4]) : 0.0;
-    if (dd >= 0 && dd <= 90 && mm >= 0 && mm <= 59 && ss >= 0 && ss <= 59){
-      dec = (sign * (dd + mm / 60.0 + ss / 3600.0)).toFixed(10);
+  if(typeof dec === 'string') {
+    var m = dec.match('^([+-])?([0-9]?[0-9]):([0-5]?[0-9][.0-9]*):?([.0-9]+)?$');
+    if (m) {
+      var sign = m[1] === '-' ? -1 : 1;
+      var dd = parseInt(m[2], 10);
+      var mm = parseFloat(m[3]);
+      var ss = m[4] ? parseFloat(m[4]) : 0.0;
+      if (dd >= 0 && dd <= 90 && mm >= 0 && mm <= 59 && ss >= 0 && ss <= 59) {
+        dec = (sign * (dd + mm / 60.0 + ss / 3600.0)).toFixed(10);
+      }
     }
   }
   return dec;
