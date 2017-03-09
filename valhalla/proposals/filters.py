1855e504219d1a7cc0b542b72b2d8e299ecc02aa
Austin Riba <ariba@lco.global>
2017-03-09 10:28:56 -0800

    Add public field to semester to allow fitlering of private semesters

--- valhalla/proposals/filters.py
+++ valhalla/proposals/filters.py
@@ -5,7 +5,7 @@ from valhalla.proposals.models import Semester, Proposal
 
 class ProposalFilter(django_filters.FilterSet):
     semester = django_filters.ModelChoiceFilter(
-        label="Semester", distinct=True, queryset=Semester.objects.all().order_by('-start')
+        label="Semester", distinct=True, queryset=Semester.objects.filter(public=True).order_by('-start')
     )
     active = django_filters.ChoiceFilter(choices=((0, 'Inactive'), (1, 'Active')), empty_label='All')
 
