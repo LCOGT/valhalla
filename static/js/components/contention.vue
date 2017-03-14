b92b84885987a6ecbea2fd2312782f5a8807186a
Austin Riba <austin@m51.io>
2017-03-14 12:45:34 -0700

    more contention plot improvements

--- static/js/components/contention.vue
+++ static/js/components/contention.vue
@@ -6,13 +6,14 @@
     <option value="2M0-FLOYDS-SCICAM">2m FLOYDS</option>
     <option value="0M4-SCICAM-SBIG">.4m SBIG</option>
   </select>
+  <i class="fa fa-spin fa-spinner" v-show="contention.length < 1"></i>
+  <center>Right Ascension</center>
   <div id="plot"></div>
 </div>
 </template>
 <script>
 import vis from 'vis';
 import $ from 'jquery';
-import moment from 'moment';
 import 'vue-style-loader!vis/dist/vis.css';
 export default {
   name: 'contention',
@@ -22,12 +23,10 @@ export default {
       contention: [],
       options: {
         style: 'bar',
-        barChart: {align:'right'},
         moveable: false,
         stack: true,
         showMajorLabels: false,
-        drawPoints: false,
-        autoResize: true,
+        drawPoints: true,
         dataAxis: {
           left: {
             range: {
@@ -47,15 +46,20 @@ export default {
       var groups = new vis.DataSet();
       for(var ra in this.contention){
         for(var prop in this.contention[ra]){
+          var y = this.contention[ra][prop] / 3600;
           items.add({
             group: prop,
             x: Number(ra) * 1000,
-            y: this.contention[ra][prop] / 3600,
+            y: y,
+            label: {
+              content: prop + ': ' + y.toFixed(2),
+              className: 'plot-label'
+            }
           });
           if(!groups.get(prop)){
             groups.add({
               id: prop,
-              content: prop
+              content: prop,
             });
           }
         }
@@ -64,11 +68,12 @@ export default {
     },
   },
   created: function(){
-    this.instrument = '0M4-SCICAM-SBIG';
+    this.instrument = '1M0-SCICAM-SINISTRO';
   },
   watch: {
     instrument: function(instrument){
       var that = this;
+      that.contention = [];
       $.getJSON('/api/contention/' + instrument + '/', function(data){
         that.contention = data;
         that.graph.setItems(new vis.DataSet());
@@ -80,9 +85,19 @@ export default {
     }
   },
   mounted: function(){
+    var that = this;
     this.graph = new vis.Graph2d(this.$el, this.toVis, this.groups, this.options);
+    this.graph.on('changed', function(){
+      $(that.$el).find('.vis-point').each(function(){
+        $(this).attr('title', $(this).next().text());
+        $(this).tooltip({'container': 'body', 'placement': 'top'});
+      });
+    });
   }
 };
 </script>
 <style>
+.plot-label {
+  display: none;
+}
 </style>
--- static/js/tools.vue
+++ static/js/tools.vue
@@ -3,6 +3,11 @@
   <div class="col-md-12">
     <h1>Planning Tools</h1>
     <h3>Network Contention</h3>
+    <p>
+        This plot shows the amount of telescope time requested by RA and instrument.
+        Observations requested where contention is high are more likely to
+        incur scheduling conflicts.
+    </p>
     <Contention></Contention>
   </div>
 </div>
