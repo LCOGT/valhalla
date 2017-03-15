8e637b2fe127234613c2bb8cdc2315e2870aeeb3
Austin Riba <ariba@lco.global>
2017-03-15 16:16:30 -0700

    set console backend default for sending mail

--- valhalla/settings.py
+++ valhalla/settings.py
@@ -184,6 +184,7 @@ ACCOUNT_ACTIVATION_DAYS = 7
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
 EMAIL_USE_TLS = True
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
