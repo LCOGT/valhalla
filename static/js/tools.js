fa6346aa40e9d3b2f051ed5e86aad2c87048bbc4
vlall <vlall@lco.global>
2017-04-03 17:12:18 -0700

    Fixing contention graph

--- static/js/tools.js
+++ static/js/tools.js
@@ -1,9 +1,73 @@
 import Vue from 'vue';
-import Tools from './tools.vue';
+import Chart from 'chart.js';
 
-new Vue({
+var app = new Vue({
   el: '#app',
-  render: function(h){
-    return h(Tools);
+  data: {
+    instrument: "1M0-SCICAM-SINISTRO",
+  },
+  created: function(){
+      this.api_call(function(keys, values) {
+      var ctx = document.getElementById("myChart");
+      var allData = {
+          type: 'bar',
+          data: {
+              labels: keys,
+              datasets: [{
+                  data: values,
+                  backgroundColor:
+                      'rgba(255, 99, 132, 0.7)',
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              legend: {
+              display: false
+          },
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true,
+                          scaleSteps:.25
+                      },
+                      barPercentage:1,
+                      categoryPercentage:.95,
+                      scaleLabel: {
+                          display: true,
+                          labelString: 'Total Requested Hours'
+                      }
+                  }],
+                  xAxes: [{
+                      ticks: {
+                          stepSize: 2
+                      },
+                  barPercentage:1,
+                  categoryPercentage:.9
+                  }],
+              },
+          }
+      }
+      myChart = new Chart(ctx, allData);
+      });
+    },
+  methods: {
+    api_call: function(callback){
+      $.getJSON("http://localhost:8000/api/contention/"+ this.instrument, function (json) {
+        var keys = Object.keys(json).map(function(key) {
+          return key;
+        });
+        var values = Object.values(json).map(function(values) {
+          return (values["All Proposals"] / 3600).toFixed(3);
+        });
+        callback(keys, values)
+      });
+    },
+    update_chart: function(callback){
+      this.api_call(function(keys, values) {
+      myChart.data.labels = keys;
+      myChart.data.datasets[0].data = values; 
+      myChart.update(); 
+    });
+    }
   }
 });
