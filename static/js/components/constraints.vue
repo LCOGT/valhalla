4a60e4057a229c96e8cd8f4d31a4a1528037c43b
nvolgenau <nvolgenau@lcogt.net>
2017-04-17 14:18:05 -0700

    Add to desc of airmass

--- static/js/components/constraints.vue
+++ static/js/components/constraints.vue
@@ -18,7 +18,8 @@
       <div :class="show ? 'col-md-6' : 'col-md-12'">
         <form class="form-horizontal">
           <customfield v-model="constraints.max_airmass" label="Maximum Airmass" field="max_airmass"
-                       v-on:input="update" :errors="errors.max_airmass" desc="Airmass = 1 at zenith">
+                       v-on:input="update" :errors="errors.max_airmass" desc="Maximum acceptable airmass (X) for scheduling.
+                       X = sec(zenith angle)">
           </customfield>
           <customfield v-model="constraints.min_lunar_distance" label="Min. Lunar Separation"
                        field="min_lunar_distance" v-on:input="update" :errors="errors.min_lunar_distance"
