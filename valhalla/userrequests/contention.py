030092dfc7ebe7be35663d1c3b71497435ea58a6
Elisabeth Heinrich-Josties <eheinrich@eheinrich-linux2.lco.gtn>
2017-05-01 23:35:01 +0000

    first attempt to fix and speed up pressure plot

--- valhalla/userrequests/contention.py
+++ valhalla/userrequests/contention.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 import math
 
 from valhalla.userrequests.models import Request
-from valhalla.common.rise_set_utils import get_rise_set_intervals, get_site_rise_set_intervals
+from valhalla.common.rise_set_utils import get_rise_set_intervals_for_site
 from valhalla.common.configdb import configdb
 
 
@@ -49,87 +49,80 @@ class Contention(object):
 class Pressure(object):
     def __init__(self, instrument_name=None, site=None, anonymous=True):
         self.anonymous = anonymous
-        self.site = site
+        self.now = timezone.now()
         self.requests = self._requests(instrument_name, site)
-        self.request_visibility = self._request_visibility()
-        self.site_nights = self._site_nights()
+        self.site = site
+        self.sites = self._sites()
 
     def _requests(self, instrument_name, site):
         requests = Request.objects.filter(
-            windows__start__lte=timezone.now() + timedelta(days=1),
-            windows__end__gte=timezone.now(),
+            windows__start__lte=self.now + timedelta(days=1),
+            windows__end__gte=self.now,
             state='PENDING',
             target__type='SIDEREAL'
         )
         if instrument_name:
-            requests = requests.filter(molecules__instrument_name=instrument_name)
+            requests = requests.filter(molecules__instrument_name=instrument_name).distinct()
 
         return requests.prefetch_related(
             'molecules', 'windows', 'target', 'constraints', 'location', 'user_request', 'user_request__proposal'
         )
 
-    def _request_visibility(self):
-        vis = {}
-        for req in self.requests:
-            if req.id not in vis:
-                vis[req.id] = sum((s - r).seconds for s, r in get_rise_set_intervals(req.as_dict) if r > timezone.now())
-        return vis
-
-    def _site_nights(self):
-        site_nights = {}
+    def _sites(self):
         if self.site:
-            sites = [{'code': self.site}]
+            return [{'code': self.site}]
         else:
-            sites = configdb.get_site_data()
-        for site in sites:
-            site_nights[site['code']] = get_site_rise_set_intervals(
-                timezone.now(), timezone.now() + timedelta(hours=24), site['code']
-            )
+            return configdb.get_site_data()
 
-        return site_nights
-
-    def _available_sites(self, start, end):
-        sites = set()
-        for site in self.site_nights:
-            for rise_set in self.site_nights[site]:
-                if rise_set[0] <= start and rise_set[1] >= end:
-                    sites.add(site)
-        return sites
-
-    def _binned_pressure_by_hours_from_now(self):
-        #  Quarter hour bins
-        quarter_hour_bins = [{} for x in range(0, 24 * 4)]
-        for x in range(0, 24 * 4):
-            start = timezone.now() + timedelta(minutes=15 * x)
-            end = timezone.now() + timedelta(minutes=15 * (x + 1))
-            available_sites = self._available_sites(start, end)
-            requests = [
-                r for r in self.requests if
-                min(w.start for w in r.windows.all()) <= start and max(w.end for w in r.windows.all()) >= end
-            ]
-            for request in requests:
-                proposal = request.user_request.proposal.id
-                pressure = self._pressure_for_request(request, available_sites)
-                if not quarter_hour_bins[x].get(proposal):
-                    quarter_hour_bins[x][proposal] = pressure
-                else:
-                    quarter_hour_bins[x][proposal] += pressure
-
-        return quarter_hour_bins
-
-    def _pressure_for_request(self, request, available_sites):
-        visible_seconds = self.request_visibility[request.id]
-        if visible_seconds < 1:
-            return 0
-        duration = request.duration
+    def _n_telescopes_available_at_site(self, site, request):
         total_tel = 0
         for molecule in request.molecules.all():
             telescopes_for_instrument_type = configdb.get_telescopes_per_instrument_type(molecule.instrument_name)
-            total_tel += min([len(t) for t in telescopes_for_instrument_type if t.site in available_sites], default=0)
-        if total_tel < 1:
-            return 0
+            total_tel += min([len(t) for t in telescopes_for_instrument_type if t.site is site], default=0)
         avg_telescopes = total_tel / request.molecules.count()
-        return (duration / visible_seconds) / avg_telescopes
+        return avg_telescopes
+
+    def _visible_intervals(self, request):
+        visible_intervals = []
+        for site in self.sites:
+            if not request.location.site or request.location.site is site:
+                intervals = get_rise_set_intervals_for_site(request.as_dict, site['code'])
+                for r, s in intervals:
+                    if (s-r).seconds >= request.duration and r >= self.now:
+                        visible_intervals.append(dict(site=site['code'], interval=(r, s)))
+        return visible_intervals
+
+    def _total_time_visible(self, request_intervals):
+        # TODO: Compensate for overlap at different sites.
+        return sum([(i['interval'][1] - i['interval'][0]).seconds for i in request_intervals])
+
+    def _binned_pressure_by_hours_from_now(self):
+        quarter_hour_bins = [{} for x in range(0, 24 * 4)]
+        bin_start_times = [self.now + timedelta(minutes=15 * x) for x in range(0, 24 * 4)]
+
+        for request in self.requests:
+            visible_intervals = self._visible_intervals(request)
+            total_time_visible = self._total_time_visible(visible_intervals)
+            if total_time_visible < 1:
+                continue
+            base_pressure = request.duration / total_time_visible
+
+            for i, bin_start in enumerate(bin_start_times):
+                n_telescopes = 0
+                for interval in visible_intervals:
+                    if interval['interval'][0] <= bin_start < interval['interval'][1]:
+                        n_telescopes += self._n_telescopes_available_at_site(interval['site'], request)
+                if n_telescopes < 1:
+                    continue
+                pressure = base_pressure / n_telescopes
+                proposal = request.user_request.proposal.id
+                # TODO: If self.site is set, only add on pressure from requests that are visible from there.
+                if not quarter_hour_bins[i].get(proposal):
+                    quarter_hour_bins[i][proposal] = pressure
+                else:
+                    quarter_hour_bins[i][proposal] += pressure
+
+        return quarter_hour_bins
 
     def _anonymize(self, data):
         for index, time in enumerate(data):
