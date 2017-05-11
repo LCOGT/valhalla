a9beeeab012669b1c790c2704a4382dc31208fd8
Austin Riba <ariba@lco.global>
2017-05-11 10:43:17 -0700

    Add ordering filter to home page, add target name filter to homepage

--- valhalla/userrequests/filters.py
+++ valhalla/userrequests/filters.py
@@ -4,13 +4,28 @@ from valhalla.userrequests.models import UserRequest, Request, Location
 
 
 class UserRequestFilter(filters.FilterSet):
-    created_after = django_filters.DateTimeFilter(name='created', lookup_expr='gte', label="Submitted after")
-    created_before = django_filters.DateTimeFilter(name='created', lookup_expr='lte', label="Submitted before")
+    created_after = django_filters.DateTimeFilter(name='created', lookup_expr='gte', label='Submitted after')
+    created_before = django_filters.DateTimeFilter(name='created', lookup_expr='lte', label='Submitted before')
     state = django_filters.ChoiceFilter(choices=UserRequest.STATE_CHOICES)
-    title = django_filters.CharFilter(name='group_id', lookup_expr='icontains', label="Title contains")
-    user = django_filters.CharFilter(name='submitter__username', lookup_expr='icontains', label="Username contains")
+    title = django_filters.CharFilter(name='group_id', lookup_expr='icontains', label='Title contains')
+    user = django_filters.CharFilter(name='submitter__username', lookup_expr='icontains', label='Username contains')
     telescope_class = django_filters.ChoiceFilter(
         choices=Location.TELESCOPE_CLASSES, name='requests__location__telescope_class'
+    )
+    target = django_filters.CharFilter(
+            name='requests__target__name', lookup_expr='icontains', label='Target name contains'
+    )
+    o = django_filters.OrderingFilter(
+        fields=(
+            ('group_id', 'title'),
+            ('modified', 'modified'),
+            ('created', 'created'),
+            ('requests__windows__end', 'end')
+        ),
+        field_labels={
+            'requests__windows__end': 'End of window',
+            'modified': 'Last Update'
+        }
     )
 
     class Meta:
