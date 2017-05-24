92d30db63a0248cbdf605592a3f26529c254b087
Austin Riba <ariba@lco.global>
2017-05-24 10:18:00 -0700

    Use true/false instead of 0/1 for proposal active filter

--- valhalla/proposals/filters.py
+++ valhalla/proposals/filters.py
@@ -8,7 +8,7 @@ class ProposalFilter(django_filters.FilterSet):
     semester = django_filters.ModelChoiceFilter(
         label="Semester", distinct=True, queryset=Semester.objects.filter(public=True).order_by('-start')
     )
-    active = django_filters.ChoiceFilter(choices=((0, 'Inactive'), (1, 'Active')), empty_label='All')
+    active = django_filters.ChoiceFilter(choices=((False, 'Inactive'), (True, 'Active')), empty_label='All')
 
     class Meta:
         model = Proposal
