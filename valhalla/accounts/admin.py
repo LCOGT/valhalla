9638efb18b5ddc6d375bd31dd67cc7ac6899ce99
Austin Riba <austin@m51.io>
2017-06-12 13:56:14 -0700

    Add logentry admin class so we can view who changed what in the admin

--- valhalla/accounts/admin.py
+++ valhalla/accounts/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from valhalla.accounts.models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.admin.models import LogEntry
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    readonly_fields = [f.name for f in LogEntry._meta.get_fields()]
+    list_display = [f.name for f in LogEntry._meta.get_fields()]
+    actions = None
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class UserProfileInline(admin.StackedInline):
@@ -13,5 +26,7 @@ class UserProfileInline(admin.StackedInline):
 class UserAdmin(AuthUserAdmin):
     inlines = [UserProfileInline]
 
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(LogEntry, LogEntryAdmin)
