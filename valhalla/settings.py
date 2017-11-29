7b60255b25a74ab95bde50231f4cae0eb7302f56
Jon Nation <jnation@lco.global>
2017-11-29 15:16:54 -0800

    removed unused import

--- valhalla/settings.py
+++ valhalla/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import sys
 from lcogt_logging import LCOGTFormatter
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
