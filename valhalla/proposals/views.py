d1a5d9824381315bf73af15ac463964a85edc129
Austin Riba <ariba@lco.global>
2018-02-15 10:53:17 -0800

    Rename semester detail to semester admin

--- valhalla/proposals/views.py
+++ valhalla/proposals/views.py
@@ -115,8 +115,9 @@ class MembershipDeleteView(LoginRequiredMixin, DeleteView):
         return Membership.objects.filter(proposal__in=proposals)
 
 
-class SemesterDetailView(UserPassesTestMixin, DetailView):
+class SemesterAdminView(UserPassesTestMixin, DetailView):
     model = Semester
+    template_name = 'proposals/semester_admin.html'
 
     def test_func(self):
         return self.request.user.is_staff
