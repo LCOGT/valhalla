032b832b94c35c01cc88cbd2cb512b93e4dd3ac8
Austin Riba <ariba@lco.global>
2018-02-16 11:23:35 -0800

    Semester detail page

--- valhalla/proposals/urls.py
+++ valhalla/proposals/urls.py
@@ -3,12 +3,13 @@ from django.views.decorators.http import require_POST
 
 from valhalla.proposals.views import ProposalDetailView, ProposalListView, InviteCreateView
 from valhalla.proposals.views import MembershipDeleteView, SemesterAdminView, MembershipLimitView
-from valhalla.proposals.views import GlobalMembershipLimitView
+from valhalla.proposals.views import GlobalMembershipLimitView, SemesterDetailView
 
 app_name = 'proposals'
 urlpatterns = [
     url(r'^membership/(?P<pk>.+)/delete/$', MembershipDeleteView.as_view(), name='membership-delete'),
     url(r'^membership/(?P<pk>.+)/limit/$', MembershipLimitView.as_view(), name='membership-limit'),
+    url(r'^semester/(?P<pk>.+)/$', SemesterDetailView.as_view(), name='semester-detail'),
     url(r'^semesteradmin/(?P<pk>.+)/$', SemesterAdminView.as_view(), name='semester-admin'),
     url(r'^$', ProposalListView.as_view(), name='list'),
     url(r'^(?P<pk>.+)/invite/$', require_POST(InviteCreateView.as_view()), name='invite'),
--- valhalla/proposals/views.py
+++ valhalla/proposals/views.py
@@ -115,6 +115,16 @@ class MembershipDeleteView(LoginRequiredMixin, DeleteView):
         return Membership.objects.filter(proposal__in=proposals)
 
 
+class SemesterDetailView(DetailView):
+    model = Semester
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['proposals'] = self.get_object().proposals.filter(active=True) \
+            .distinct().order_by('tag__name')
+        return context
+
+
 class SemesterAdminView(UserPassesTestMixin, DetailView):
     model = Semester
     template_name = 'proposals/semester_admin.html'
