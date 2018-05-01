85b7394c08c6d1c4bab5306fa7fca39d8f1a0f9a
Austin Riba <ariba@lco.global>
2018-05-01 14:41:17 -0700

    Use gunicorn and whitenoise instead of uwsgi for deployment

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,16 @@ MAINTAINER Austin Riba <ariba@lco.global>
 EXPOSE 80
 ENV NODE_ENV production
 WORKDIR /valhalla
-CMD uwsgi --ini /etc/uwsgi.ini
+CMD gunicorn valhalla.wsgi -b 0.0.0.0:80
 
 RUN curl -sL https://deb.nodesource.com/setup_9.x | bash -
 RUN apt-get install -y gfortran nodejs
 
 COPY requirements.txt .
-RUN pip install numpy && pip install uwsgi -r requirements.txt
+RUN pip install numpy && pip install gunicorn -r requirements.txt
 
 COPY package.json package-lock.json ./
 RUN npm install && npm install --only=dev
-
-COPY docker/uwsgi.ini /etc/
 
 COPY . /valhalla
 RUN npm run build
--- valhalla/settings.py
+++ valhalla/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -195,6 +196,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
