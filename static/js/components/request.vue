cf8658e26087a550825504c3ba8c55b4b288130d
Elisabeth Heinrich-Josties <eheinrichjosties@gmail.com>
2018-05-23 02:20:03 +0000

    available_instruments depends on a call to the api and rarely populates before the call to instrumentToDataType, making data_type almost always be IMAGE. Determine data_type from instrument_name instead.

--- static/js/components/request.vue
+++ static/js/components/request.vue
@@ -128,8 +128,8 @@ export default {
       }
     }, 300),
     instrumentToDataType: function(value){
-      if(!_.isEmpty(this.available_instruments) && value) {
-        return this.available_instruments[value].type;
+      if (value.includes('NRES') || value.includes('FLOYDS')) {
+        return 'SPECTRA';
       }
       return 'IMAGE';
     },
